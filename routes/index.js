bb504506262a3fe9ed31db876be47ce56b2b6d4f
earnedmarketing <tomkisss@gmail.com>
2015-07-12 18:02:27 +0100

    Revert "Autenticacion de usuarios"
    This reverts commit 67c34c581f7171e4319d15444fc8049270f31ca6.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
-var sessionController = require('../controllers/session_controller');
 
 // Página de entrada (home page)
 router.get('/', function(req, res) {
@@ -12,11 +11,6 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
-
-// Definición de rutas de sesion
-router.get('/login',  sessionController.new);     // formulario login
-router.post('/login', sessionController.create);  // crear sesión
-router.get('/logout', sessionController.destroy); // destruir sesión
 
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
@@ -28,8 +22,7 @@ router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
 router.put('/quizes/:quizId(\\d+)',        quizController.update);
 router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
 
-// Definición de rutas de comentarios
-router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
-router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/new',            commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',              commentController.create);
 
 module.exports = router;
